88d983aa1f0b94886d33a1292de534943616652c
Karen <kjgamis@live.ca>
2018-04-12 13:48:28 -0400

    instance variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
 
-  # def welcome
-  #   render :about
-  # end
+  def welcome
+    @header = "Welcome user"
+  end
+
+  def about
+    @header = "This is the about page"
+  end
 
 end
